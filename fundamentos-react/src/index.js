7433e857bb0894de63b606a4dd24ab2bee25ecf8
icaro nogueira <icaronogueira@deltaglobal.com.br>
2025-01-02 07:56:19 -0300

    exibindo string na tela e sintaxe JSX

---
+++ fundamentos-react/src/index.js
@@ -0,0 +1,13 @@
+import ReactDOM from 'react-dom/client';
+import React from 'react';
+
+const tag = <h2>tagggggg</h2>
+
+const div = ReactDOM.createRoot(document.getElementById('root'))
+div.render(
+    <div>
+        Olá josnelllllll
+        <br></br>
+        { tag }
+    </div>
+)
