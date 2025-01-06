8dfd96814b384fcdb9563a8225b0c8e8a29f316b
icaro nogueira <icaronogueira@deltaglobal.com.br>
2025-01-06 09:19:07 -0300

    componente card 1

--- fundamentos-react/src/App.jsx
+++ fundamentos-react/src/App.jsx
@@ -4,6 +4,7 @@ import Primeiro from './components/basicos/Primeiro'
 import ComParametro from './components/basicos/ComParametro';
 import Fragmento from './components/basicos/Fragmento';
 import NumeroAleatorio from './components/basicos/NumeroAleatorio';
+import Card from './components/layout/Card';
 
 export function App(props) {
     return (
@@ -11,17 +12,38 @@ export function App(props) {
             <h1>Fundamentos React</h1>
             Olá josnelllllll
             <br></br>
-            <NumeroAleatorio
-                min={10}
-                max={20}
-            ></NumeroAleatorio>
-            <Fragmento></Fragmento>
-            <ComParametro
-                situacao='situacao do aluno'
-                aluno='Pedro'
-                nota={9.3}
-            ></ComParametro>
-            <Primeiro></Primeiro>
+
+            <Card 
+                titulo='#04 - Desafio aleatório'
+            >
+                <NumeroAleatorio
+                    min={10}
+                    max={20}
+                ></NumeroAleatorio>
+            </Card>
+
+            <Card 
+                titulo='#03 - Fragmento'
+            >
+                <Fragmento></Fragmento>
+            </Card>
+
+            <Card 
+                titulo='#02 - Parametro'
+            >
+                <ComParametro
+                    situacao='situacao do aluno'
+                    aluno='Pedro'
+                    nota={9.3}
+                ></ComParametro>
+            </Card>
+
+            <Card 
+                titulo='#01 - Primeiro componente'
+            >
+                <Primeiro></Primeiro>
+            </Card>
+
         </div>
     )
 }
