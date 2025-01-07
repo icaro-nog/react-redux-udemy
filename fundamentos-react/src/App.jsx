cd2bceb13785e430ce573d067d1d9156c7328023
icaro nogueira <icaronogueira@deltaglobal.com.br>
2025-01-07 07:45:34 -0300

    componente card 02

--- fundamentos-react/src/App.jsx
+++ fundamentos-react/src/App.jsx
@@ -5,45 +5,48 @@ import ComParametro from './components/basicos/ComParametro';
 import Fragmento from './components/basicos/Fragmento';
 import NumeroAleatorio from './components/basicos/NumeroAleatorio';
 import Card from './components/layout/Card';
+import './App.css'
 
 export function App(props) {
     return (
-        <div id='app'>
+        <div className='App'>
             <h1>Fundamentos React</h1>
             Olá josnelllllll
             <br></br>
 
-            <Card 
-                titulo='#04 - Desafio aleatório'
-            >
-                <NumeroAleatorio
-                    min={10}
-                    max={20}
-                ></NumeroAleatorio>
-            </Card>
+            <div className='Cards'>
+                <Card 
+                    titulo='#04 - Desafio aleatório'
+                    color='#080'
+                >
+                    <NumeroAleatorio
+                        min={10}
+                        max={20}
+                    ></NumeroAleatorio>
+                </Card>
 
-            <Card 
-                titulo='#03 - Fragmento'
-            >
-                <Fragmento></Fragmento>
-            </Card>
+                <Card 
+                    titulo='#03 - Fragmento'
+                >
+                    <Fragmento></Fragmento>
+                </Card>
 
-            <Card 
-                titulo='#02 - Parametro'
-            >
-                <ComParametro
-                    situacao='situacao do aluno'
-                    aluno='Pedro'
-                    nota={9.3}
-                ></ComParametro>
-            </Card>
+                <Card 
+                    titulo='#02 - Parametro'
+                >
+                    <ComParametro
+                        situacao='situacao do aluno'
+                        aluno='Pedro'
+                        nota={9.3}
+                    ></ComParametro>
+                </Card>
 
-            <Card 
-                titulo='#01 - Primeiro componente'
-            >
-                <Primeiro></Primeiro>
-            </Card>
-
+                <Card 
+                    titulo='#01 - Primeiro componente'
+                >
+                    <Primeiro></Primeiro>
+                </Card>
+            </div>
         </div>
     )
 }
