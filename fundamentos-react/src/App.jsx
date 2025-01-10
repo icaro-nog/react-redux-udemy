9916fb89b0994aa73c6e4240d316af3b5235d5f2
icaro nogueira <icaronogueira@deltaglobal.com.br>
2025-01-10 08:53:31 -0300

    componente controlado

--- fundamentos-react/src/App.jsx
+++ fundamentos-react/src/App.jsx
@@ -14,6 +14,7 @@ import ParOuImpar from './components/condicional/ParOuImpar';
 import UsuarioInfo from './components/condicional/UsuarioInfo';
 import DiretaPai from './components/comunicacao/DiretaPai';
 import IndiretaPai from './components/comunicacao/IndiretaPai';
+import Input from './components/formulario/Input';
 
 export function App(props) {
     return (
@@ -25,7 +26,14 @@ export function App(props) {
             <div className='Cards'>
 
                 <Card
-                    titulo='#09 - Comunicação indireta'
+                    titulo='#11 - Input'
+                    color='#311b92'
+                >
+                    <Input></Input>
+                </Card>
+
+                <Card
+                    titulo='#10 - Comunicação indireta'
                     color='#f50057'
                 >
                     <IndiretaPai></IndiretaPai>
---
+++ fundamentos-react/src/components/formulario/Input.jsx
@@ -0,0 +1,38 @@
+import React, { useState } from "react";
+import './Input.css'
+
+const Input = props => {
+
+    const [valor, setValor] = useState('Inicial')
+
+    function quandoMudar(e){
+        setValor(e.target.value)
+    }
+
+    return (
+        <div className="Input">
+            <h2>{valor}</h2>
+            <div style={{
+                display: 'flex',
+                flexDirection: 'column'
+            }}>
+                <input
+                    value={valor}
+                    onChange={quandoMudar}
+                ></input>
+
+                <input
+                    value={valor}
+                    readOnly
+                ></input>
+
+                <input
+                    value={undefined}
+                ></input>
+
+            </div>
+        </div>
+    )
+}
+
+export default Input;
