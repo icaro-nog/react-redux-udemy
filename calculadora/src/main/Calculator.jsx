760be4fd8e328887e11fcb8b0c07b97aa31aa65d
icaro nogueira <icaronogueira@deltaglobal.com.br>
2025-01-15 08:30:07 -0300

    componente button 02

--- calculadora/src/main/Calculator.jsx
+++ calculadora/src/main/Calculator.jsx
@@ -5,7 +5,24 @@ import Button from '../components/Button';
 import Display from "../components/Display";
 
 class Calculator extends Component {
+
+    clearMemory(){
+        console.log('limpar')
+    }
+
+    setOperation(operation){
+        console.log(operation)
+    }
+
+    addDigit(n){
+        console.log(n)
+    }
+
     render() {
+
+        const addDigit = n => this.addDigit(n)
+        const setOperation = op => this.setOperation(op)
+
         return (
             <div className="calculator">
                 <Display
@@ -13,54 +30,71 @@ class Calculator extends Component {
                 ></Display>
                 <Button
                     label="AC"
+                    click={() => this.clearMemory()}
                 ></Button>
                 <Button
                     label="/"
+                    click={setOperation}
                 ></Button>
                 <Button
                     label="7"
+                    click={addDigit}
                 ></Button>
                 <Button
                     label="8"
+                    click={addDigit}
                 ></Button>
                 <Button
                     label="9"
+                    click={addDigit}
                 ></Button>
                 <Button
                     label="*"
+                    click={setOperation}
                 ></Button>
                 <Button
                     label="4"
+                    click={addDigit}
                 ></Button>
                 <Button
                     label="5"
+                    click={addDigit}
                 ></Button>
                 <Button
                     label="6"
+                    click={addDigit}
                 ></Button>
                 <Button
                     label="-"
+                    click={setOperation}
                 ></Button>
                 <Button
                     label="1"
+                    click={addDigit}
                 ></Button>
                 <Button
                     label="2"
+                    click={addDigit}
                 ></Button>
                 <Button
                     label="3"
+                    click={addDigit}
                 ></Button>
                 <Button
                     label="+"
+                    click={setOperation}
                 ></Button>
                 <Button
                     label="0"
+                    click={addDigit}
                 ></Button>
                 <Button
                     label="."
+                    click={setOperation}
                 ></Button>
                 <Button
                     label="="
+                    click={setOperation}
                 ></Button>
             </div>
         )
