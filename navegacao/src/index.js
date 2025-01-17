c8c18a021071bd0518c2bcd0608f7a9f16a6ec32
icaro nogueira <icaronogueira@deltaglobal.com.br>
2025-01-17 07:25:33 -0300

    estrutura projeto

--- navegacao/src/index.js
+++ navegacao/src/index.js
@@ -1,8 +1,10 @@
 import ReactDOM from 'react-dom/client';
 import React from 'react';
+import App from './views/App';
 
 
 const div = ReactDOM.createRoot(document.getElementById('root'))
 div.render(
+    <App></App>,
     <h1>apppp</h1>
 )
