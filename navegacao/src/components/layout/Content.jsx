c9ab7d04c5b811ff97ecfca0e6cd09b654970dec
icaro nogueira <icaronogueira@deltaglobal.com.br>
2025-01-21 08:19:16 -0300

    página não encontrada

--- navegacao/src/components/layout/Content.jsx
+++ navegacao/src/components/layout/Content.jsx
@@ -4,6 +4,7 @@ import { Routes, Route } from "react-router-dom"; // Importe esses arquivos
 import About from "../../views/examples/About";
 import Home from "../../views/examples/Home";
 import Param from "../../views/examples/Param";
+import NotFound from "../../views/examples/NotFound";
  
 const Content = props => (
   <main className="Content">
@@ -20,6 +21,10 @@ const Content = props => (
         path="/" 
         element={<Home />} 
         exact
+      />
+      <Route 
+        path="*" 
+        element={<NotFound />} 
       />
     </Routes>
   </main>
