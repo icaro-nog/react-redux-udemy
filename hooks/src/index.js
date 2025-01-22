b6282ba358def1d4039dec341b0e90e653d0e8e4
icaro nogueira <icaronogueira@deltaglobal.com.br>
2025-01-22 08:20:25 -0300

    configuração projeto hooks

---
+++ hooks/src/index.js
@@ -0,0 +1,9 @@
+import './index.css'
+import React from 'react'
+import App from './views/App'
+import { createRoot } from "react-dom/client"
+
+const container = document.getElementById("root")
+const root = createRoot(container)
+
+root.render(<App></App>)
