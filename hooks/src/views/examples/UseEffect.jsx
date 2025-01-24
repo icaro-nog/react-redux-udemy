7d5657f057fe6ee824572086720e778247e6986e
icaro nogueira <icaronogueira@deltaglobal.com.br>
2025-01-24 08:36:21 -0300

    useEffect parImpar

--- hooks/src/views/examples/UseEffect.jsx
+++ hooks/src/views/examples/UseEffect.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import PageTitle from '../../components/layout/PageTitle'
+import SectionTitle from '../../components/layout/SectionTitle'
 
 function calcFatorial(num){
 
@@ -10,14 +11,24 @@ function calcFatorial(num){
     return calcFatorial(n -1) * n
 }
 
+function validaParImpar(valor){
+    const n = parseInt(valor);
+
+    return (n % 2 === 0);
+}
+
 const UseEffect = (props) => {
 
     const [number, setNumber] = useState(1);
     const [fatorial, setFatorial] = useState(1);
 
+    const [parImpar, setParImpar] = useState(0);
+    const [status, setStatus] = useState('Ímpar');
+
     useEffect(function(){
         setFatorial(calcFatorial(number))
-    }, [number])
+        setStatus(validaParImpar(parImpar))
+    }, [number, parImpar])
 
     return (
         <div className="UseEffect">
@@ -26,6 +37,9 @@ const UseEffect = (props) => {
                 subtitle="Permite executar efeitos colaterais em componentes funcionais!"
             />
 
+            <SectionTitle
+                title="Exercício #01"
+            ></SectionTitle>
             <div className="center">
                 <div>
                     <span
@@ -41,6 +55,29 @@ const UseEffect = (props) => {
                     value={number}
                     onChange={e => setNumber(e.target.value)}
                 />
+            </div>
+
+            <SectionTitle
+                title="Exercício #02"
+            ></SectionTitle>
+            <div
+                className='center'
+            >
+                <div>
+                    <span
+                        className='text'
+                    >Status:</span>
+                    <span
+                        className='text red'
+                    >{status ? 'Par' : 'Ímpar'}</span>
+                </div>
+                <input 
+                    type="number" 
+                    className='input'
+                    value={parImpar}
+                    onChange={e => setParImpar(e.target.value)}
+                />
+
             </div>
         </div>
 
