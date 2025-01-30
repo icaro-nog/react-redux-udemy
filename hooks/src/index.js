60bc3986bef4d9bedd778019bfbe4a2abbc9fc9c
icaro nogueira <icaronogueira@deltaglobal.com.br>
2025-01-30 08:31:29 -0300

    useContext 01

--- hooks/src/index.js
+++ hooks/src/index.js
@@ -6,4 +6,6 @@ import { createRoot } from "react-dom/client"
 const container = document.getElementById("root")
 const root = createRoot(container)
 
-root.render(<App></App>)
+root.render(
+    <App></App>
+)
