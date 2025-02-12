7fa419831ad6ac98b0eb4edcae1d1654c187b0c0
icaro nogueira <icaronogueira@deltaglobal.com.br>
2025-02-12 08:20:02 -0300

    navegacao entre componentes

--- aulao_nextjs/fundamentos/src/pages/estiloso.jsx
+++ aulao_nextjs/fundamentos/src/pages/estiloso.jsx
@@ -1,8 +1,12 @@
 import '../styles/Estiloso.modules.css'
+import Link from 'next/link'
 
 export default function Estiloso(){
     return (
         <div className='roxo'>
+            <Link
+                href="/"
+            >Index</Link>
             <h1>Estilo usando CSS Módulos</h1>
         </div>
     )
