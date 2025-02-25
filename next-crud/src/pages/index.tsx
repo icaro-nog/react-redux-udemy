cfce38945116f84b42c6d12cb40a5eb7b1734571
Ícaro Nogueira <icaronogueira@deltaglobal.com.br>
2025-02-25 08:12:33 -0300

    componentes Layout e Titulo

--- next-crud/src/pages/index.tsx
+++ next-crud/src/pages/index.tsx
@@ -1,12 +1,18 @@
+import Layout from "@/components/Layout"
+
 export default function Home() {
   return (
     <>
       <div
-        className={`flex h-screen justify-center items-center bg-gradient-to-r from-purple-500 to-blue-600`}
+        className={`
+            flex justify-center items-center h-screen
+            bg-gradient-to-r from-blue-500 to-purple-500
+            text-white
+          `}
       >
-        <span>
-          texto
-        </span>
+        <Layout titulo="Cadastro Simples">
+          <span>Conteúdo</span>
+        </Layout>
       </div>
     </>
   );
